f176f55aa2e9fff7de5a1700bdd02ac156a0b78d
xiedandan <xiedandan@hikvision.com.cn>
2022-10-20 16:49:58 +0800

    feat: 🍬 引入 element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-// import Cesium from './components/Cesium.vue'
-</script>
+
 
 <template>
-  <router-view></router-view>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <!-- <Cesium /> -->
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-container>
+        <el-aside width="250px" class="aside">
+          <el-menu
+            active-text-color="#ffd04b"
+            background-color="#545c64"
+            class="el-menu-vertical-demo"
+            default-active="1"
+            text-color="#fff"
+          >
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><Position /></el-icon>
+                <span>VU3基础</span>
+              </template>
+              <router-link to="/">
+                <el-menu-item index="1-1">item one</el-menu-item>
+              </router-link>
+            </el-sub-menu>
+            <router-link to="/cesium">
+              <el-menu-item  index="2"> 
+                  <el-icon><MapLocation /></el-icon>
+                  <span>Cesium</span>
+              </el-menu-item>
+            </router-link>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
-
-<style scoped>
+<script setup lang="ts">
+  import RandExp from "randexp";
+  const randexp = new RandExp(/.+/);
+   console.log(randexp.gen());
+  import { Position, MapLocation } from '@element-plus/icons-vue'
+</script>
+<style scoped lang="less">
+.common-layout{
+  .header{
+    line-height: 60px;
+    background: #272822;
+    color: #fff;
+    font-size: 20px;
+  }
+  .aside{
+    height: calc(100vh - 60px);
+    background: #545c64;
+  }
+}
 .logo {
   height: 6em;
   padding: 1.5em;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+// import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css';
+// import * as Icons from '@element-plus/icons-vue';
+
 import './style.css'
 import App from './App.vue'
 import Router from './router'
-createApp(App).use(Router).mount('#app');
+createApp(App)
+.use(Router)
+.use(ElementPlus)
+// .use(Icons)
+.mount('#app');
