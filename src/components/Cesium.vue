130f33cfdab1c76c7f613978508558d6f7fc9a5c
xiedandan <xiedandan@hikvision.com.cn>
2022-10-20 10:28:17 +0800

    feat:🍬  vue3，cesium

---
+++ src/components/Cesium.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="mainMap"></div>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNTFlNWVmMC1jOWRhLTQyMDktOTY4Ny02YTU5YWFlMGYwY2IiLCJpZCI6OTQzNDQsImlhdCI6MTY1Mjg4ODEyNn0.g2vq1qq_rdfZeRTy73nBkEDzhMIM4upkYcbIdFYnCiQ'
+
+  const viewer = new Cesium.Viewer('mainMap', {
+    infoBox: false,
+    homeButton: false,
+    timeline: false,
+    navigationHelpButton: false,
+    geocoder: false,
+    sceneModePicker: false,
+    animation: false,
+    fullscreenButton: false
+  })
+  const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings())
+  viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(116.405419, 39.926666, 3000),
+    orientation: {
+      heading: Cesium.Math.toRadians(0.0),
+      pitch: Cesium.Math.toRadians(-25.0)
+    }
+  })
+  // viewer.cesiumWidget.creditContainer.style.display = 'none'
+})
+</script>
+<style scoped>
+#mainMap {
+  width: 800px;
+  height: 800px;
+  background: #aabbcc;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import cesium from 'vite-plugin-cesium' // 引入插件
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), cesium()]
 })
